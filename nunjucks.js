f10fa90d686ae50e728bd045ede1eeeb88c38bfc
jonelet <jonelet@gmail.com>
2017-07-15 12:38:08 +0200

    init

---
+++ nunjucks.js
@@ -0,0 +1,11 @@
+webpackJsonp([2],{
+
+/***/ 4:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/home/jonelet/programowanie/projects/web-challenge/web-challenge-35/src/nunjucks.js'");
+
+/***/ })
+
+},[4]);
+//# sourceMappingURL=nunjucks.js.map
---
+++ sass.js
@@ -0,0 +1,11 @@
+webpackJsonp([2],{
+
+/***/ 7:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[7]);
+//# sourceMappingURL=sass.js.map
